005ba9f3ed57ef30908b357e20999264ef3f0bb4
Pratima <prataggie@gmail.com>
2017-10-13 01:03:18 -0400

    second commit, deliverable 2

---
+++ src/calculateDays.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+public class calculateDays {
+	public static void main (String [] args) {
+		Scanner scnr = new Scanner(System.in);
+		
+		int i = 0;
+		/*constants*/
+		/*final int MTHJAN = 31;
+		int MTHFEB = 28;
+		final int MTHMAR = 31;
+		final int MTHAPR = 30;
+		final int MTHMAY = 31;
+		final int MTHJUN = 30;
+		final int MTHJUL = 31;
+		final int MTHAUG = 31;
+		final int MTHSEP = 30;
+		final int MTHOCT = 31;
+		final int MTHNOV = 30;
+		final int MTHDEC = 31;*/
+		
+		int todaDate = 0;
+		int compDate = 0;
+		
+		int todaYear = 0;
+		int compYear = 0;
+		int todaMth = 0;
+		int compMth = 0;
+		int todaDay = 0;
+		int compDay = 0;
+		String todaDateStr = "";
+		String compDateStr = "";
+		String todaYearStr = "";
+		String compYearStr = "";
+		String todaMthStr = "";
+		String compMthStr = "";
+		String todaDayStr = "";
+		String compDayStr = "";
+			
+		/*userinput dates 
+		System.out.println("Please enter today's date in the following format: MMDDYYYY.");
+		/*todaDate = scnr.nextInt();
+		todaDateStr = scnr.nextLine();
+		System.out.println("Please enter the date you want to start your calculations from in the following format: MMDDYYYY.");
+		/*compDate = scnr.nextInt();
+		compDateStr = scnr.nextLine();
+		
+		System.out.print(todaDateStr + " ");
+		System.out.print(compDateStr + " "); */
+		
+		System.out.println("Please enter today's Year in the following format: YYYY.");
+		todaYearStr = scnr.nextLine();
+		System.out.println("Please enter today's Month in the following format: MM.");
+		todaMthStr = scnr.nextLine();
+		System.out.println("Please enter today's Day/Date in the following format: DD.");
+		todaDayStr = scnr.nextLine();
+		System.out.println("Please enter past date Year in the following format: YYYY.");
+		compYearStr = scnr.nextLine();
+		System.out.println("Please enter past date Month in the following format: MM.");
+		compMthStr = scnr.nextLine();
+		System.out.println("Please enter past date Day/Date in the following format: DD.");
+		compDayStr = scnr.nextLine();
+		/*compDate = scnr.nextInt();
+		compDateStr = scnr.nextLine();*/
+		
+		System.out.print(todaYearStr + " " + todaMthStr + " " + todaDayStr + " ");
+		System.out.println(compYearStr + " " + compMthStr + " " + compDayStr);
+		
+		
+		todaDateStr = "10122017";
+		compDateStr = "04201978"; // need to make sure single digit become double digit*/
+		
+		/*todaYear = todaDate % 10000;
+		todaDate = todaDate / 10000;
+		compYear = compDate % 10000;
+		compDate = compDate / 10000;
+		System.out.println("Years: " + todaYear + " " + compYear + " ");
+		System.out.println(todaDate + " " + compDate + " "); 
+		
+		todaDay = todaDate % 100;
+		todaMth = todaDate / 100;
+		compDay = todaDate % 100;
+		compMth = compDate / 100;
+		System.out.println("Days: " + todaDay + " " + compDay + " ");
+		System.out.println("Months: " + todaMth + " " + compMth + " ");*/
+		
+		i = 12
+		while (todaMthStr <= String) {
+			if (todaMthStr = )
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
